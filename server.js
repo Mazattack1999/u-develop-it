b6c8c73036a21eabb3c2ad8ff7027dbc28a1e34e
micah_z <micah.a.zahn@gmail.com>
2022-05-10 16:43:50 -0500

    connect to election database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,11 +9,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'root',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running, see http://localhost:${PORT}`);
+    console.log(`Server running, see http://localhost:${PORT}.`);
 });
